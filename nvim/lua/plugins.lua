cc232a237baa6a58b54a88732505048c5ad848a1
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2024-05-13 18:31:28 +0100

    Fixed many issues with the nvim config, now it bootstraps correctly

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,10 +17,6 @@ vim.g.loaded_netrwPlugin = 1
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-
   use 'nvim-tree/nvim-web-devicons'
 
   use {
@@ -36,6 +32,8 @@ return require('packer').startup(function(use)
         }
       }
 
+      vim.keymap.set('n', '<Leader>n', '<Cmd>NvimTreeToggle<CR>')
+
     end
   }
 
@@ -47,53 +45,49 @@ return require('packer').startup(function(use)
     end,
     config = function()
       require('wincent.commandt').setup({
-        -- Customizations go here.
+        vim.keymap.set('n', '<Leader>b', '<Plug>(CommandTBuffer)');
+        vim.keymap.set('n', '<Leader>t', '<Plug>(CommandT)');
       })
     end,
   }
-  vim.keymap.set('n', '<Leader>b', '<Plug>(CommandTBuffer)')
-  vim.keymap.set('n', '<Leader>t', '<Plug>(CommandT)')
-  vim.keymap.set('n', '<Leader>n', '<Cmd>NvimTreeToggle<CR>')
 
-  vim.cmd([[
-    augroup packer_user_config
-      autocmd!
-      autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
-  ]])
-
-  use 'ishan9299/nvim-solarized-lua'
-  vim.g.solarized_italics = 1
-  vim.cmd('colorscheme solarized')
+  use {
+    'ishan9299/nvim-solarized-lua',
+    config = function()
+      vim.g.solarized_italics = 1
+      vim.cmd('colorscheme solarized')
+    end,
+  }
 
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function()
+
+        require'nvim-treesitter.configs'.setup {
+          -- A list of parser names, or "all" (the five listed parsers should always be installed)
+          ensure_installed = { "cpp", "c", "lua", "vim", "vimdoc", "query", "python", "perl", "ruby" },
+
+          -- Install parsers synchronously (only applied to `ensure_installed`)
+          sync_install = false,
+
+          -- Automatically install missing parsers when entering buffer
+          -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+          auto_install = true,
+
+          highlight = {
+            enable = true,
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+          }
+        }
+
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
     end,
-  }
-
-  require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "cpp", "c", "lua", "vim", "vimdoc", "query", "python", "perl", "ruby" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  }
   }
 
   use {
@@ -110,27 +104,32 @@ return require('packer').startup(function(use)
   use { 'mhartington/formatter.nvim' ,
     config = function()
             -- Utilities for creating configurations
-local util = require "formatter.util"
+            --
+    local util = require "formatter.util"
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    cpp = {
-            require("formatter.filetypes.cpp").clangformat,
-    },
-    c = {
-            require("formatter.filetypes.cpp").clangformat,
-    },
-
-  }
-}
+    -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+    require("formatter").setup {
+      -- Enable or disable logging
+      logging = true,
+      -- Set the log level
+      log_level = vim.log.levels.WARN,
+      -- All formatter configurations are opt-in
+      filetype = {
+        cpp = { 
+          require("formatter.filetypes.cpp").clangformat,
+        },
+        c = {
+          require("formatter.filetypes.cpp").clangformat,
+        },
+      }
+    }
     end
   }
 
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
 end)
+
